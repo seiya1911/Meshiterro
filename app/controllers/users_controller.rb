3c3829e5bb3caa8c5d9e7d6365d0d120cdcb89d7
seiya1911 <n.siy1911cb@gmail.com>
2021-05-06 10:11:47 +0000

    [Start]4-7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @post_image = @user.post_image.page(params[:page]).reverse_order
+    @post_images = @user.post_image.page(params[:page]).reverse_order
   end
 
   def edit
